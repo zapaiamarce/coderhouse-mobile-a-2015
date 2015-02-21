d502f6d4e9f23e75da633beabf9bc007c540b349
Coderhouse <coderhouse@Marcelos-MacBook-Pro.local>
2015-02-21 16:33:27 -0300

    Commit message

---
+++ clases/clase12/clase12.js
@@ -0,0 +1,29 @@
+
+var onLoad = function(){
+	window.miForm = document.querySelector('form');
+	window.lista = document.querySelector('ul.lista');
+	
+	// lista.addEventListener('mouseover',function(event){
+	// 	console.log(this)
+		
+	// })
+
+	// document.body.addEventListener('mouseover',function(e){
+	// 	console.log('this',this)
+	// 	console.log('target',e.target);
+	// })
+
+	miForm.addEventListener('submit',function(e){
+		if(e.target.algoQueElUsuarioPuso.value != 'enviar'){
+			e.preventDefault();
+		}
+	})
+
+	// document.body.addEventListener('mouseover',function(e){
+	// 	console.log('this',this)
+	// 	console.log('target',e.target);
+	// })
+
+}
+
+window.addEventListener('load',onLoad);
